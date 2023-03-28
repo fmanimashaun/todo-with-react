3971971726c43b110eafcc0ea3260625c7acd552
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-28 18:30:33 +0100

    Render tasks on the page

--- src/components/TodoLogic.jsx
+++ src/components/TodoLogic.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
 
-const TodoLogic = () => <div>TodoLogic content</div>;
+const TodoLogic = () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ];
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <input type="checkbox" checked={todo.completed} />
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default TodoLogic;
