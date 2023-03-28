cec96b9ecbfa3e3717dec0d6d78387d59966975b
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-28 14:47:09 +0100

    Update setup

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-const TodoApp = () => {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-      <p>I am in a React Component!</p>
-    </div>
-  );
-};
+const TodoApp = () => (
+  <>
+    <h1>Hello world!</h1>
+    <p>I am in a React Component!</p>
+  </>
+);
+
 export default TodoApp;
-
