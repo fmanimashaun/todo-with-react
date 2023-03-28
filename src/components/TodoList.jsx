48003e493466c0275d1faa7b3ef677a79be51ae5
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-28 19:33:35 +0100

    Create the TodoList component

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+const TodoList = ({ todos }) => (
+  <ul>
+    {todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} />
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      completed: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default TodoList;
