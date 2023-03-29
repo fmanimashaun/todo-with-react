fd9d5899036aa8906996d71bb44a2fe90978e168
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-29 13:28:05 +0100

    Create a Form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Form = () => (
+  <>
+    <h1>React Form Handling</h1>
+    <form action="#" method="#">
+      <label htmlFor="firstName">
+        First Name: <input type="text" name="firstName" id="firstName" />
+      </label>
+    </form>
+  </>
+);
+
+export default Form;
