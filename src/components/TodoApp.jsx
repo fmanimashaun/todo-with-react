60415da3224dd11807450665fa658623b76c4496
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-29 20:49:22 +0100

    Remove other unrelated components from TodoApp

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -2,16 +2,12 @@ import React from 'react';
 import Header from './Header';
 import TodoLogic from './TodoLogic';
 import PageHeader from './PageHeader';
-import Modal from './Modal';
-import Form from './Form';
 
 const TodoApp = () => (
   <>
     <PageHeader />
     <Header />
     <TodoLogic />
-    <Modal />
-    <Form />
   </>
 );
 
