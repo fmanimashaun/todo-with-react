567e6dbc11f1afe40afd298b7dac2fa7b89c781f
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-29 18:03:43 +0100

    Update the Form component with select tag

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Form = () => (
-  <>
-    <h1>React Form Handling</h1>
-    <form action="#" method="#">
-      <label htmlFor="firstName">
-        First Name: <input type="text" name="firstName" id="firstName" />
-      </label>
-    </form>
-  </>
-);
+const Form = () => {
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    message: '',
+    carBrand: '',
+  });
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const carBrands = ['Mercedes', 'BMW', 'Maserati', 'Infinity', 'Audi'];
+  let id = 0;
+  const carBrandOptions = carBrands.map((carBrand) => {
+    id += 1;
+    return (
+      <option value={carBrand} key={id}>
+        {carBrand}
+      </option>
+    );
+  });
+  return (
+    <>
+      <h1>React Form Handling</h1>
+      <form action="#" method="#">
+        <label htmlFor="firstName">
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            value={user.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="lasttName">
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            id="lastName"
+            value={user.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label htmlFor="message">
+          Message:
+          <textarea
+            name="message"
+            id="message"
+            onChange={handleChange}
+            defaultValue={user.message}
+          />
+        </label>
+        <br />
+        <label htmlFor="carBrand">
+          Car Brand:
+          <select
+            name="carBrand"
+            id="carBrand"
+            value={user.carBrand}
+            onChange={handleChange}
+          >
+            <option value="" disabled>--Select a car brand--</option>
+            {carBrandOptions}
+          </select>
+        </label>
+      </form>
+      <h5>
+        Name:
+        {user.firstName}
+        {user.lastName}
+        <br />
+        Message:
+        {user.message}
+      </h5>
+    </>
+  );
+};
 
 export default Form;
